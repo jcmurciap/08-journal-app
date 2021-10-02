ddd51ff7fc4d078a9b0b29e7fc4f1a4cf355312e
enigma <camilo.murcia.pimentel@gmail.com>
2021-10-02 14:03:50 -0500

    added main user interface types methods

---
+++ src/components/actions/ui.js
@@ -0,0 +1,19 @@
+import { types } from '../types/types';
+
+export const setError = ( err ) => ({
+    type: types.uiSetError,
+    payload: err,
+});
+
+export const removeError = () => ({
+    type: types.uiRemoveError,
+});
+
+export const startLoading = () => ({
+    type: types.uiStartLoading,
+}); 
+
+export const finishLoading = () => ({
+    type: types.uiFinishLoading,
+});
+
