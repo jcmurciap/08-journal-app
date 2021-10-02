0a64f2d839478050765376b0b52739ffac87e59b
enigma <camilo.murcia.pimentel@gmail.com>
2021-10-02 15:34:44 -0500

    added authentication router

---
+++ src/routers/AuthRouter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { RegisterScreen } from '../components/auth/RegisterScreen';
+
+export const AuthRouter = () => {
+    
+    return (
+        <div className="auth__main">
+            <div className="auth__box-container">
+                <Switch>
+                    <Route path="/auth/login" component={ LoginScreen }/>
+                    <Route path="/auth/register" component={ RegisterScreen }/>
+                    <Redirect to="/auth/login"/>
+                </Switch>
+            </div>
+        </div>
+    )
+}
+
