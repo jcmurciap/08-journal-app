6fbf5f6d7fd1ba5dde4e21974c28bedd71b692a8
enigma <camilo.murcia.pimentel@gmail.com>
2021-10-02 16:09:52 -0500

    added types tests

---
+++ src/tests/components/types/types.test.js
@@ -0,0 +1,28 @@
+import { types } from "../../../components/types/types"
+
+describe( '<types /> tests', () => {
+    
+    
+    test( 'Shoult return skeleton', () => {
+        const skeleton = types;
+        expect( skeleton ).toEqual({
+            login: '[Auth] Login',
+            logout: '[Auth] Logout',
+            uiSetError: '[UI] set Error',
+            uiRemoveError: '[UI] Remove Error',
+            uiStartLoading: '[UI] Start loading',
+            uiFinishLoading: '[UI] Finish loading',
+            notesAddNew: '[Notes] New note',
+            notesActive: '[Notes] Set active note',
+            notesLoad: '[Notes] Load notes',
+            notesUpdated: '[Notes] Updated note',
+            notesFileUrl: '[Notes] Updated image url',
+            notesDelete: '[Notes] Delete note',
+            notesLogoutCleaning: '[Notes] Logout Cleaning',
+        })
+    })
+} )
+
+
+
+
