3a56687223ac64d2a19e538a8e6ba08eb6e60b4d
enigma <camilo.murcia.pimentel@gmail.com>
2021-10-02 15:00:39 -0500

    added global types configuration component

---
+++ src/components/types/types.js
@@ -0,0 +1,20 @@
+
+export const types = {
+
+    login: '[Auth] Login',
+    logout: '[Auth] Logout',
+    uiSetError: '[UI] set Error',
+    uiRemoveError: '[UI] Remove Error',
+
+    // blocking login botton
+    uiStartLoading: '[UI] Start loading',
+    uiFinishLoading: '[UI] Finish loading',
+    notesAddNew: '[Notes] New note',
+    notesActive: '[Notes] Set active note',
+    notesLoad: '[Notes] Load notes',
+    notesUpdated: '[Notes] Updated note',
+    notesFileUrl: '[Notes] Updated image url',
+    notesDelete: '[Notes] Delete note',
+    notesLogoutCleaning: '[Notes] Logout Cleaning',   
+
+}
