9db78c13a565560eeaacbb378a5d08e79afe4949
enigma <camilo.murcia.pimentel@gmail.com>
2021-10-02 21:14:58 -0500

    fixing onScroll bug in startUploading test

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,7 @@ import {createSerializer} from 'enzyme-to-json';
 
 Enzyme.configure({ adapter: new Adapter() });
 expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
+
+// usado para la prueba de 'startUploading' should update entry URL
+//const noScroll = () => {}
+//Object.defineProperty(window, 'scrollTo', {value: noScroll, writable: true})
