45c7cf7b396e6430a66097283e603c9e6edc4e29
enigma <camilo.murcia.pimentel@gmail.com>
2021-10-04 11:40:57 -0500

    added startLoginEmailPassword method test

--- src/tests/components/actions/auth.test.js
+++ src/tests/components/actions/auth.test.js
@@ -3,7 +3,7 @@ import configureStore from 'redux-mock-store'; //ES6 modules
 import thunk from 'redux-thunk';
 import '@testing-library/jest-dom';
 
-import { logout,login, startLogout } from "../../../components/actions/auth"
+import { logout,login, startLogout, startLoginEmailPassword } from "../../../components/actions/auth"
 import { types } from "../../../components/types/types";
 
 const middlewares = [thunk];
@@ -44,19 +44,38 @@ describe( '<auth /> actions test', () => {
                 displayName:'Camilo',
             }
         })
-    })
+    });
 
     test('startLogout succesfully', async() => {
 
         await store.dispatch(startLogout());
         const actions = store.getActions();
-        console.log(actions);   
         expect(actions[0]).toEqual({type: types.logout});
         expect(actions[1]).toEqual({type: types.notesLogoutCleaning});
 
-    })
+    });
 
+    test( 'should login effectivelly in startLoginEmailPassword method', async() => {
 
+        await store.dispatch(startLoginEmailPassword('test@test.com', '123456'));
+        const actions = store.getActions();
+
+        expect(actions[0]).toEqual({
+            type: types.uiStartLoading
+        });
+        
+        expect(actions[1]).toEqual({
+            type: types.uiFinishLoading
+        });
+        
+        expect(actions[2]).toEqual({
+            type: types.login,
+            payload: {
+                uid: 123,
+                displayName: 'user123'
+            }
+        });
+    });
 
 
 
