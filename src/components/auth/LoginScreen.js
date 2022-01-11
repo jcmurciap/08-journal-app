e41317d1fe6defe1d059a6dbfb45425a3aa8a758
enigma <camilo.murcia.pimentel@gmail.com>
2022-01-10 22:25:57 -0500

    Add login screen component

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,4 +1,3 @@
-// libraries
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -13,10 +12,7 @@ export const LoginScreen = () => {
     
     const { loading } = useSelector( state => state.ui); // REVISE COMO FUNCIONA 
     
-    const [ formValues, handleInputChange ] = useForm({
-        email: 'camilo@gmail.com',
-        password: '123',
-    }); 
+    const [ formValues, handleInputChange ] = useForm({}); 
     
     const { email, password } = formValues;
         
@@ -31,61 +27,60 @@ export const LoginScreen = () => {
     
     return (
         <>
-            <h3 className="auth__title">
-                Sign In
-            </h3>
-            <form 
-                onSubmit={ handleLogin }
-                className="animate__animated animate__fadeIn animate__faster"
-            >
-                <input 
-                    autoComplete="off"
-                    className="auth__input"
-                    name="email"
-                    onChange={ handleInputChange }    
-                    placeholder="Email"
-                    type="text"
-                    value={ email }
-                />
-                <input 
-                    className="auth__input"
-                    name="password" 
-                    onChange={ handleInputChange }  
-                    placeholder="Password"
-                    type="password"
-                    value={ password } 
-                />
-            
-                <button 
-                    className="btn btn-primary btn-block"
-                    disabled={ loading }
-                    type="submit"
+            <div className='auth__container'>
+                <h3 className="auth__title">
+                    Sign In
+                </h3>
+                <form 
+                    onSubmit={ handleLogin }
+                    className="animate__animated animate__fadeIn animate__faster"
                 >
-                    Login
-                </button>
+                    <input 
+                        autoComplete="off"
+                        className="auth__input"
+                        name="email"
+                        onChange={ handleInputChange }    
+                        placeholder="Email"
+                        type="text"
+                        value={ email }
+                    />
+                    <input 
+                        className="auth__input"
+                        name="password" 
+                        onChange={ handleInputChange }  
+                        placeholder="Password"
+                        type="password"
+                        value={ password } 
+                    />
                 
-                <div className="auth__social-networks">
-                    <p>Login with social networks</p>
-                    <div 
-                        className="google-btn"
-                        onClick={ handleGoogleLogin }
+                    <button 
+                        className="btn btn-primary btn-block"
+                        disabled={ loading }
+                        type="submit"
                     >
-                        <div className="google-icon-wrapper">
-                            <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+                        Login
+                    </button>
+                    
+                    <div className="auth__social-networks">
+                        <p>Login with social networks</p>
+                        <div 
+                            className="google-btn"
+                            onClick={ handleGoogleLogin }
+                        >
+                            <div className="google-icon-wrapper">
+                                <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+                            </div>
+                            <p className="btn-text">
+                                <b>Sign In with google</b>
+                            </p>
                         </div>
-                        <p className="btn-text">
-                            <b>Sign In with google</b>
-                        </p>
                     </div>
-                </div>
-                
-                <Link 
-                    to="/auth/register"
-                    className="link"
-                >
-                    Create new account
-                </Link>
-            </form>
+                    
+                    <Link to="/auth/register" className="link">
+                        Create new account
+                    </Link>
+                </form>
+            </div>
         </>
     )
 }
