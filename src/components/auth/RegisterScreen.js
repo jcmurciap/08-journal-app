3e46041d3b5dc6b156948ea8c5901ec7e3015774
enigma <camilo.murcia.pimentel@gmail.com>
2022-01-10 22:27:34 -0500

    Add register screen component to new users

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -9,23 +9,15 @@ import { startRegisterWithEmailPasswordName } from '../actions/auth';
 export const RegisterScreen = () => {
    
     const dispatch = useDispatch(); 
-    const {  msgError } = useSelector( state => state.ui );
+    const {msgError} = useSelector(state => state.ui);
     
-    const [ formValues, handleInputChange ] = useForm({
-        fname: 'jose',
-        email: 'jose@gmail.com',
-        password: 'camilo',
-        password2: 'camilo',
-    });
+    const [ formValues, handleInputChange ] = useForm({});
     const { fname, email, password, password2 } = formValues;
     
-    
-
     const handleRegister = (e) => {
         e.preventDefault();
         
         if ( isFormValid() ){
-
             dispatch( startRegisterWithEmailPasswordName( fname, email, password ) )
         }
     }
@@ -41,76 +33,78 @@ export const RegisterScreen = () => {
             dispatch(setError('Passwords needs to be equal and has at least 6 characters.\n\tTry again'));
             return false;
         }
-        dispatch( removeError() );
+        dispatch(removeError());
         return true;
     }; 
 
     return (
         <>
-            <h3 className="auth__title">
-                Create New Account
-            </h3>
+            <div className='auth__container'>
+                <h3 className="auth__title">
+                    Create New Account
+                </h3>
 
-            <form 
-                onSubmit={ handleRegister }
-                className="animate__animated animate__fadeIn animate__faster"
-            >
-                {
-                    msgError && (
-                        <div className="auth__alert-error">
-                            { msgError }
-                        </div>
-                    )
-                }
-                <input 
-                    type="text"
-                    placeholder="Name"
-                    name="fname"
-                    className="auth__input"
-                    autoComplete="off"
-                    value={ fname }    
-                    onChange={ handleInputChange }
-                />
-                <input 
-                    type="text"
-                    placeholder="Email"
-                    name="email"
-                    className="auth__input"
-                    autoComplete="off"
-                    value={ email }    
-                    onChange={ handleInputChange }
-                />
-                <input 
-                    type="password"
-                    placeholder="Password"
-                    name="password" 
-                    className="auth__input"
-                    value={ password }   
-                    onChange={ handleInputChange }
-                />
-                <input 
-                    type="password"
-                    placeholder="Confirm Password"
-                    name="password2" 
-                    className="auth__input"
-                    value={ password2 }   
-                    onChange={ handleInputChange }
-                />
-            
-                <button 
-                    type="submit"
-                    className="btn btn-primary btn-block mb-5"
+                <form 
+                    onSubmit={handleRegister}
+                    className="animate__animated animate__fadeIn animate__faster"
                 >
-                    Register
-                </button>
-                
-                <Link 
-                    to="/auth/login"
-                    className="link"
-                >
-                    Already registered?
-                </Link>
-            </form>
+                    {
+                        msgError && (
+                            <div className="auth__alert-error">
+                                { msgError }
+                            </div>
+                        )
+                    }
+                    <input 
+                        type="text"
+                        placeholder="Name"
+                        name="fname"
+                        className="auth__input"
+                        autoComplete="off"
+                        value={ fname }    
+                        onChange={ handleInputChange }
+                    />
+                    <input 
+                        type="text"
+                        placeholder="Email"
+                        name="email"
+                        className="auth__input"
+                        autoComplete="off"
+                        value={ email }    
+                        onChange={ handleInputChange }
+                    />
+                    <input 
+                        type="password"
+                        placeholder="Password"
+                        name="password" 
+                        className="auth__input"
+                        value={ password }   
+                        onChange={ handleInputChange }
+                    />
+                    <input 
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="password2" 
+                        className="auth__input"
+                        value={ password2 }   
+                        onChange={ handleInputChange }
+                    />
+
+                    <button 
+                        type="submit"
+                        className="btn btn-primary btn-block mb-5"
+                    >
+                        Register
+                    </button>
+
+                    <Link 
+                        to="/auth/login"
+                        className="link"
+                    >
+                        Already registered?
+                    </Link>
+                </form>
+            </div>
         </>
     )
 }
